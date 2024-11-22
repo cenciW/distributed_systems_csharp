9dd9f0d8f515ad954c40601aa9ef3f5aaf2b07ec
Jose Augusto Cenci Castilho <ze.ccenci@gmail.com>
2024-11-22 17:45:12 +0000

    feat: starting Ficha17

---
+++ Ficha17/App_Code/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Vendas
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://www.estgp.pt/Vendas")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ProductService : System.Web.Services.WebService
+    {
+
+        public ProductService()
+        {
+
+            //Uncomment the following line if using designed components 
+            //InitializeComponent(); 
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+    }
+}
