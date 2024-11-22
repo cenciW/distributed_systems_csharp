d6c8ab8cc6a102ea45e85c7bb37cdde20130427d
Jose Augusto Cenci Castilho <ze.ccenci@gmail.com>
2024-11-22 09:24:39 +0000

    Add ErrorRecord class with XML serialization annotations
    Introduced `ErrorRecord` class in the Ficha14 namespace to handle error logging. The class includes properties with XML serialization attributes for `ReportedBy`, `AffectedComponent`, `ErrorDescription`, `CorrectiveActions`, and `Status`. Default values are provided in the constructor.

---
+++ Ficha14/ErrorRecord.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+
+namespace Ficha14
+{
+    [XmlRoot("RegistroErro")]
+    public class ErrorRecord
+    {
+        [XmlElement("DetectadoPor")]
+        public string ReportedBy { get; set; }
+
+        [XmlElement("ComponenteAfetado")]
+        public string AffectedComponent { get; set; }
+
+        [XmlElement("Descricao")]
+        public string ErrorDescription { get; set; }
+
+        [XmlArray("ListaAccaoCorrecao")]
+        [XmlArrayItem("AccaoCorrecao")]
+        public List<string> CorrectiveActions { get; set; }
+
+        [XmlElement("Estado")]
+        public string Status { get; set; }
+
+        public ErrorRecord()
+        {
+            CorrectiveActions = new List<string>();
+            Status = "Por resolver";
+            ReportedBy = "Desconhecido";
+            AffectedComponent = "Desconhecido";
+            ErrorDescription = "Desconhecido";
+        }
+    }
+}
